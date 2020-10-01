a6eef5ca22a43b1c8dac234c933cf8997a2331ac
Nhi Cung <nhcung@knox.edu>
2020-10-01 09:58:58 +0700

    Set up language switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Page() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => changeLanguage('de')}>de</button>
+        <button onClick={() => changeLanguage('en')}>en</button>
+      </div>
+      <div>{t('description.part2')}</div>
     </div>
   );
 }
 
-export default App;
+// function App() {
+
+//   return (
+//     <Suspense fallback='loading'>
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <button onClick={() => changeLanguage('de')}>de</button>
+//           <button onClick={() => changeLanguage('en')}>en</button>
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//         </a>
+//         </header>
+//       </div>
+//     </Suspense>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
+  return (
+    <Suspense fallback="loading">
+      <Page />
+    </Suspense>
+  );
+}
