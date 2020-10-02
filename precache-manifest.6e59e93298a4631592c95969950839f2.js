458624c16973ed3538a0023db6c92e85e2434d0b
Nhi Cung <nhcung@knox.edu>
2020-10-02 09:43:09 +0700

    Updates

---
+++ precache-manifest.6e59e93298a4631592c95969950839f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609af5126835cd2a3b36e01147739fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa37b62001f39aa08d2",
+    "url": "/static/css/main.171fff06.chunk.css"
+  },
+  {
+    "revision": "05ef745c1d42f109e93a",
+    "url": "/static/js/2.3aef6ec6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3aef6ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa37b62001f39aa08d2",
+    "url": "/static/js/main.e8684655.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0ad94d8e01bec0532af032b217138773.js"
+  "/precache-manifest.6e59e93298a4631592c95969950839f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
