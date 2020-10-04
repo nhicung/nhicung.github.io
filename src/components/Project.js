9ad7f6bd5efdacda0de02228f950718ec18c316b
Nhi Cung <nhcung@knox.edu>
2020-10-04 14:15:17 +0700

    Rescale project setup for my work page

--- src/components/Project.js
+++ src/components/Project.js
@@ -32,7 +32,9 @@ const useStyles = makeStyles((theme) => ({
   project: {
     color: '#ECECEC',
     textAlign: 'left',
-    paddingTop: '8%'
+    paddingTop: '8%',
+    width: 700,
+    paddingRight:100
   },
   names: {
     fontSize: '44px',
@@ -47,14 +49,20 @@ const useStyles = makeStyles((theme) => ({
   },
   descripts: {
     fontFamily: 'Montserrat',
-    fontSize: '22px'
+    fontSize: '22px',
+    textAlign:'justify'
   },
 
   button: {
     marginRight: 50,
     fontFamily: 'Roboto Slab',
   },
-
+  imgWrap:{
+    width:300,
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center'
+  },
   pic: {
     paddingLeft: '5%'
   },
@@ -70,8 +78,8 @@ export default function Project(props) {
         <Container className={classes.container} style={{ backgroundImage: props.project.bgimage }} maxWidth='lg' >
           <div className={classes.project}>
             <h1 className={classes.names}> {props.project.name}</h1>
-            <h2 className={classes.tools}id={props.project.id} > {t(`mywork.${props.project.id}.tool`) }</h2>
-            <p className={classes.descripts}> {t(`mywork.${props.project.id}.description`) }</p>
+            <h2 className={classes.tools} id={props.project.id} > {t(`mywork.${props.project.id}.tool`)}</h2>
+            <p className={classes.descripts}> {t(`mywork.${props.project.id}.description`)}</p>
             {(props.project.webURL != null) ?
               <Button
                 color="primary"
@@ -89,19 +97,21 @@ export default function Project(props) {
                 href={props.project.gitURL}> {t('projectButton.button2')} </Button> : null}
           </div>
           <Fade>
-            {(props.project.imgHover != null) ?
-              <img
-                style={{ height: props.project.height, paddingTop: props.project.padding }}
-                className={classes.pic}
-                src={props.project.img}
-                onMouseOver={e => (e.currentTarget.src = props.project.imgHover)}
-                onMouseOut={e => (e.currentTarget.src = props.project.img)}
-                alt={props.project.name} /> :
-              <img
-                style={{ height: props.project.height, paddingTop: props.project.padding }}
-                className={classes.pic}
-                src={props.project.img}
-                alt={props.project.name} />}
+            <div className={classes.imgWrap}>
+              {(props.project.imgHover != null) ?
+                <img
+                  style={{ height: props.project.height, paddingTop: props.project.padding }}
+                  className={classes.pic}
+                  src={props.project.img}
+                  onMouseOver={e => (e.currentTarget.src = props.project.imgHover)}
+                  onMouseOut={e => (e.currentTarget.src = props.project.img)}
+                  alt={props.project.name} /> :
+                <img
+                  style={{ height: props.project.height, paddingTop: props.project.padding }}
+                  className={classes.pic}
+                  src={props.project.img}
+                  alt={props.project.name} />}
+            </div>
           </Fade>
         </Container>
       </Fade>
